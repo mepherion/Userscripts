ff76ddc59b76482a05d3573f6907a5004d8be29c
mepherion <mepherion@users.noreply.github.com>
2023-08-20 08:08:13 -0400

    Update fix_vod_rich_content.user.js

--- FFZ/fix_vod_rich_content.user.js
+++ FFZ/fix_vod_rich_content.user.js
@@ -4,26 +4,12 @@
 // @updateURL https://github.com/mepherion/Userscripts/raw/main/FFZ/fix_vod_rich_content.user.js
 // @downloadURL https://github.com/mepherion/Userscripts/raw/main/FFZ/fix_vod_rich_content.user.js
 // @match       https://www.twitch.tv/videos/*
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 8/15/2023, 8:03:19 AM
 // @grant GM_addStyle
 // @run-at       document-start
 // ==/UserScript==
-
-/*
-function GM_addStylea(css) {
-  const style = document.getElementById("GM_addStyleBy8626") || (function() {
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.id = "GM_addStyleBy8626";
-    document.head.appendChild(style);
-    return style;
-  })();
-  const sheet = style.sheet;
-  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
-}
-*/
 
 GM_addStyle( `
   div.ffz-card-rich div.tw-ellipsis {
