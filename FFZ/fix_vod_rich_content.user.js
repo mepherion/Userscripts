75f07d480460b0ade07b8e68e44910de4e95678a
mepherion <mepherion@users.noreply.github.com>
2023-08-20 08:03:38 -0400

    Update fix_vod_rich_content.user.js

--- FFZ/fix_vod_rich_content.user.js
+++ FFZ/fix_vod_rich_content.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Fix FFZ Twitch VOD Chat Rich Content Wrapping
 // @namespace   Violentmonkey Scripts
-// @updateURL https://gist.github.com/mepherion/25d876f538eb055cb21a6bac2c0219b3/raw/04a09298c48402d1f11f229e0e57b2bf4e1bdb11/fix_ffz_rich_content.user.js
-// @downloadURL https://gist.github.com/mepherion/25d876f538eb055cb21a6bac2c0219b3/raw/04a09298c48402d1f11f229e0e57b2bf4e1bdb11/fix_ffz_rich_content.user.js
+// @updateURL https://github.com/mepherion/Userscripts/raw/main/FFZ/fix_vod_rich_content.user.js
+// @downloadURL https://github.com/mepherion/Userscripts/raw/main/FFZ/fix_vod_rich_content.user.js
 // @match       https://www.twitch.tv/videos/*
 // @version     1.0
 // @author      -
