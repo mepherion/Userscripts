ef7aae7065796d83959fa2e64c59f87809957963
mepherion <mepherion@users.noreply.github.com>
2024-03-27 13:11:12 -0400

    Update fix_vod_rich_content.user.js

--- FFZ/fix_vod_rich_content.user.js
+++ FFZ/fix_vod_rich_content.user.js
@@ -5,7 +5,7 @@
 // @downloadURL https://github.com/mepherion/Userscripts/raw/main/FFZ/fix_vod_rich_content.user.js
 // @match       https://www.twitch.tv/videos/*
 // @match       https://www.twitch.tv/*/video/*
-// @version     1.3
+// @version     1.4
 // @author      -
 // @description 8/15/2023, 8:03:19 AM
 // @grant GM_addStyle
@@ -14,6 +14,6 @@
 
 GM_addStyle( `
   div.ffz--chat-card {
-    max-width: calc(250px - 1.5rem)!important
+    max-width: 260px !important
   }
 ` );
